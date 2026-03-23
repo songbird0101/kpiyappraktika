d46d85da51be5493c32c3cd627d0a40ae0ab6ff2
danikkkkk <your_email@example.com>
2026-03-23 03:51:50 -0700

    20.03.2026

---
+++ Gurin 20.03.2026/z2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class Coach
+{
+    public string Name { get; set; }
+
+    public Coach(string name)
+    {
+        Name = name;
+    }
+
+    public void Train(Athlete athlete)
+    {
+        Console.WriteLine($"Тренер {Name} тренирует спортсмена {athlete.Name}");
+    }
+}
+
+class Equipment
+{
+    public string Type { get; private set; }
+
+    public Equipment(string type)
+    {
+        Type = type;
+    }
+
+    public void Use()
+    {
+        Console.WriteLine($"Используется экипировка: {Type}");
+    }
+}
+
+class Team
+{
+    public string Name { get; set; }
+
+    public Team(string name)
+    {
+        Name = name;
+    }
+
+    public void AddAthlete(Athlete athlete)
+    {
+        Console.WriteLine($"Спортсмен {athlete.Name} присоединился к команде {Name}");
+    }
+}
+
+class Athlete
+{
+    public string Name { get; set; }
+
+    private Coach[] coaches;
+
+    private Equipment equipment;
+
+    public Team Team { get; set; }
+
+    public Athlete(string name, Coach[] coaches, string equipmentType)
+    {
+        Name = name;
+        this.coaches = coaches;
+        equipment = new Equipment(equipmentType); 
+    }
+
+    public void Train()
+    {
+        Console.WriteLine($"\nСпортсмен {Name} начинает тренировку:");
+        equipment.Use();
+        foreach (var coach in coaches)
+        {
+            coach.Train(this);
+        }
+        if (Team != null)
+            Console.WriteLine($"Тренируется в команде {Team.Name}");
+        else
+            Console.WriteLine("Тренируется индивидуально");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Coach[] coaches1 = new Coach[] { new Coach("Иван"), new Coach("Петр") };
+        Coach[] coaches2 = new Coach[] { new Coach("Сергей") };
+
+        Athlete athlete1 = new Athlete("Алексей", coaches1, "Беговые кроссовки");
+        Athlete athlete2 = new Athlete("Мария", coaches2, "Спортивная форма");
+
+        Team team = new Team("Сборная города");
+        athlete1.Team = team;
+        team.AddAthlete(athlete1);
+
+        Athlete[] athletes = { athlete1, athlete2 };
+
+        foreach (var athlete in athletes)
+        {
+            athlete.Train();
+        }
+    }
+}
---
+++ Gurin 20.03.2026/z4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+interface IPowerOn
+{
+    void TogglePower();
+}
+
+interface IPowerOff
+{
+    void TogglePower();
+}
+
+class Device : IPowerOn, IPowerOff
+{
+    void IPowerOn.TogglePower()
+    {
+        Console.WriteLine("Включение устройства");
+    }
+
+    void IPowerOff.TogglePower()
+    {
+        Console.WriteLine("Выключение устройства");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Device device = new Device();
+
+        IPowerOn on = device;
+        on.TogglePower();
+
+        IPowerOff off = device;
+        off.TogglePower();
+    }
+}
